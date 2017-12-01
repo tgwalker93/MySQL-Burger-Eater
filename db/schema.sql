04c145189376d40e2891ed652af05d308b3e9fbe
tgwalker93 <tgwalker93@gmail.com>
2017-12-01 00:53:57 -0800

    test

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+
+CREATE TABLE burgers(
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows. --
+  id INTEGER(255) AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(50),
+  devoured BOOL,
+  date TIMESTAMP,
+  -- Creates a boolean column called "mastered" which will automatically fill --
+  -- with TRUE when a new row is made and the value isn't otherwise defined. --
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers
+
+
