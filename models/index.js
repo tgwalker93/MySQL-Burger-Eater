b6cbfa431ee5af3a1ff2fda89187187c10b6eb21
tgwalker93 <tgwalker93@gmail.com>
2018-01-28 01:51:20 -0800

    finished styling

--- models/index.js
+++ models/index.js
@@ -6,7 +6,6 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
 var config = require('../config/config')[env];
-// var config    = require(__dirname + '/..\config\config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
